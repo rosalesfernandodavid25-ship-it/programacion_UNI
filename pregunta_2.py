486d2216ea57584bf456d353191c98949a47e138
Fernando <rosalesfernandodavid25@gmail.com>
2026-07-21 16:05:33 -0600

    Add program to calculate average of three numbers

---
+++ pregunta_2.py
@@ -0,0 +1,6 @@
+# programar para calcular el promedio de tres numeros
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+numero3 = float(input("Ingrese el tercer número: "))
+promedio = (numero1 + numero2 + numero3) / 3
+print("El promedio de los tres números es:", promedio)
