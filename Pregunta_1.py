84f08185c35136126bbc5d71214695939d97e894
fernando David <rosalesfernandodavid25@gmail.com>
2026-08-23 21:59:02 -0600

    actualice

--- Pregunta_1.py
+++ Pregunta_1.py
@@ -3,3 +3,4 @@ base = float(input("Ingrese la base del rectángulo: "))
 altura = float(input("Ingrese la altura del rectángulo: "))
 area = base * altura
 print("El área del rectángulo es:", area)  
+ 
